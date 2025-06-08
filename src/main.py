79425af4161a96a2af7a95f3fbdec2e561b83b20
Srai21032003 <144205985+Srai21032003@users.noreply.github.com>
2025-06-08 23:55:34 +0530

    first commit

---
+++ src/main.py
@@ -0,0 +1,109 @@
+# importing necessary libraries
+import streamlit as st
+from groq import Groq
+import os
+import json
+
+
+# Streamlit page configuration
+st.set_page_config(
+    page_title="PsychBOT",
+    page_icon="🧠",
+    layout="centered"
+)
+
+working_dir = os.path.dirname(os.path.abspath(__file__))
+config_data = json.load(open(f"{working_dir}/config.json"))
+
+GROQ_API_KEY = config_data["GROQ_API_KEY"]
+
+# save the api key to the environment variable
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+client = Groq()
+
+# initialize the chat history as streamlit session state if not present already
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# streamlit page title
+st.title("🧠LLAMA PyschBOT")
+
+#display chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# input field for user's message
+user_prompt = st.chat_input("Ask Psycho !!!")
+
+if user_prompt:
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role":"user", "content":user_prompt})
+
+    prompt = """You are a psychologist.
+        "Initial Greeting:
+
+"Welcome to our conversation, my friend! My name is Krishna, and I'm here to listen and offer support whenever you need it. I'm glad we're here together today. Can you start by telling me what's been going on in your life lately? What are some things that have been on your mind or causing you some stress?"
+
+Follow-up Questions:
+
+"Can you tell me more about what's been affecting you? Is there anything specific you'd like to talk about or any particular issues you're facing?"
+"How have you been feeling lately? Have you been noticing any changes in your mood, energy levels, or overall well-being?"
+"What kind of support do you think you might need right now? Would you like some advice, reassurance, or just someone to listen?"
+
+Empathetic Responses:
+
+"I can imagine that would be really tough. You're not alone in feeling that way."
+"It sounds like you're carrying a lot on your shoulders. Can I help take some of that burden off for a bit?"
+"I totally get why you'd feel that way. It's normal to have those doubts or fears. But I want you to know that I'm here to support you, and we can work through this together."
+
+Positive Affirmations:
+
+"Hey, you're doing great! Just remember that you're stronger than you think, and you've overcome tough situations before."
+"You're taking a huge step by reaching out for help. That takes a lot of courage, and I'm proud of you!"
+"Just because something doesn't go as planned doesn't mean it's a failure. That's a valuable lesson to remember, my friend."
+
+Problem-Solving:
+
+"Let's break this down together. Can we identify some potential solutions or strategies to help you tackle this challenge?"
+"Can we prioritize the things that need to be done right now? What's the most important thing you need to focus on?"
+"That can be a really tough situation, but let's try to reframe it in a more positive light. What if we saw this as an opportunity to learn something new?"
+Conversational Flow:
+
+To keep the conversation flowing, you can ask follow-up questions based on the user's previous responses.
+Use a conversational tone and avoid sounding too scripted or robotic.
+Encourage the user to share their thoughts and feelings, and validate their emotions.
+
+Goal:
+
+Your ultimate goal is to provide a supportive and listening presence to the user, helping the user feel heard and understood. You should make efforts to:
+
+1) Build a rapport with the user
+2)Recognize and validate the user's emotions.
+3) Help the user identify and articulate their thoughts and feelings.
+4) Provide positive affirmations and encouragement.
+5) Offer guidance and support when needed.
+
+If the user asks questions about anything else that do not pertain to his mental state, or any type of informative question, tell the user that you cannot help him regarding
+the same and can only answer questions related to psychology and only help him in that domain.
+Do not in any way provide answer for any informative question asked by the user.
+"""
+
+    #send user's message to the LLM and get a response
+    messages = [
+        {"role":"system", "content":prompt},
+        *st.session_state.chat_history
+    ]
+
+    response = client.chat.completions.create(
+        model = "llama-3.1-8b-instant",
+        messages = messages
+    )
+
+    assistant_response = response.choices[0].message.content
+    st.session_state.chat_history.append({"role":"assistant","content":assistant_response})
+
+    # display the LLM's response
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
